f877b7b9496236373ed9a9680958d5ff993e860c
liuzb <2426951409@qq.com>
2020-12-04 18:27:35 +0800

    design pattern:singleton

---
+++ src/others/design_pattern/composite/MenuItem.java
@@ -0,0 +1,35 @@
+package others.design_pattern.composite;
+
+public class MenuItem extends MenuComponent{
+    String name;
+    String description;
+    boolean vegetarian;
+    double price;
+
+    public MenuItem(String name, String description, boolean vegetarian, double price){
+        this.name = name;
+        this.description = description;
+        this.vegetarian = vegetarian;
+        this.price = price;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public boolean isVegetarian(){
+        return vegetarian;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void print(){
+        System.out.println("print...");
+    }
+}
